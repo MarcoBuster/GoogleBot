aa98fbcbae8fb05ff32760746d2bb7a5ed549cd3
Marco Aceti <personale@marcoaceti.it>
2017-04-03 17:08:52 +0200

    Edited copyright notice and license on header of each file.

--- plugins/drive.py
+++ plugins/drive.py
@@ -1,24 +1,23 @@
-"""
-Copyright (c) 2017 Marco Aceti <dev@marcoaceti.it>
+# Copyright (c) 2017 Marco Aceti <dev@marcoaceti.it>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 
 from config import *
 
@@ -665,7 +664,7 @@ def getfiles(user, pagetoken=None, parent=None):
               "callback_data": "drv@fldr@" + ('root' if parent is None else parent) + '@upld'}],
             [{"text": user.getstr('back_button'),
               "callback_data": "drive" if parent != 'root' else "home"}]
-    ]
+        ]
 
     # TODO: Go to upper folder by pressing back button
 
@@ -741,7 +740,7 @@ def process_callback(bot, cb, user):
                             {"inline_keyboard": [
                                 [{"text": user.getstr('back_button'), "callback_data": "drv@fldr@" + folder}]
                             ]}
-                    )
+                        )
                 })
 
                 user.state('drive_upload_' + folder)
@@ -779,11 +778,11 @@ def process_callback(bot, cb, user):
                 bot.api.call('editMessageText', {
                     'chat_id': cb.chat.id, 'message_id': cb.message.message_id,
                     'text': user.getstr('drive_delete_done'), 'parse_mode': 'HTML', 'reply_markup':
-                    json.dumps(
-                        {"inline_keyboard": [
-                            [{"text": user.getstr('back_button'), "callback_data": "drive"}]
-                        ]}
-                    )
+                        json.dumps(
+                            {"inline_keyboard": [
+                                [{"text": user.getstr('back_button'), "callback_data": "drive"}]
+                            ]}
+                        )
                 })
 
 

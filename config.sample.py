c414fc9b52b9dd17bdd9be3595333e6185af0fc3
Marco Aceti <personale@marcoaceti.it>
2018-02-11 12:41:10 +0100

    Edited configuration, LICENSE and .gitignore file

---
+++ config.sample.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2017-2018 Marco Aceti <dev@marcoaceti.it>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+TOKEN = ''  # Bot token from t.me/BotFather
+BOT_USERNAME = ''  # Without "@"
+
+GOOGLE_USERNAME = ''  # Google account username for pytrends module
+GOOGLE_PASSWORD = ''  # Google account password for pytrends module
+
+PLOTLY_USERNAME = ''  # Plot.ly username for graphs
+PLOTLY_API_KEY = ''  # Plot.ly api key for graphs
+
+GOOGLE_OAUTH_CLIENT_ID = ''  # Google OAuth Client ID for Google OAuth 2 API
+GOOGLE_OAUTH_CLIENT_SECRET = ''  # Google OAuth Client Secret for Google OAuth 2 API
+GOOGLE_OAUTH_REDIRECT_URI = ''  # Google OAuth Redirect URI for Google OAuth 2 API
+
+GOOGLE_API_KEY = ''  # Google API key for Google APIs
